c7febc0fa1c513646c0e7bc1ec327cba6ab65d2b
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-06-01 23:10:07 +0900

    Test case where no placeholder exists in given text (#42)

--- options/src/tests/utils.test.ts
+++ options/src/tests/utils.test.ts
@@ -9,12 +9,12 @@ describe('utils', () => {
   it('findPlaceholder', () => {
     let match: RegExpMatchArray | null;
 
-    // Contains match
+    // Contains placeholder
     match = utils.findPlaceholder('func(${x})');
     expect(match?.[0]).toEqual('${x}');
     expect(match?.[1]).toEqual('x');
 
-    // Does not contain match
+    // Does not contain placeholder
     match = utils.findPlaceholder('func(x)');
     expect(match).toEqual(null);
   });
@@ -23,15 +23,18 @@ describe('utils', () => {
     let body: string;
     let ranges: utils.MatchRange[];
 
+    // No placeholder
+    [body, ranges] = utils.replacePlaceholders('func()');
+    expect(body).toEqual('func()');
+    expect(ranges).toEqual([]);
+
     // Single placeholder
     [body, ranges] = utils.replacePlaceholders('func(${x})');
-
     expect(body).toEqual('func(x)');
     expect(ranges).toEqual([{ start: 5, end: 6 }]);
 
     // Multiple placeholders
     [body, ranges] = utils.replacePlaceholders('func(${x}, ${x})');
-
     expect(body).toEqual('func(x, x)');
     expect(ranges).toEqual([
       { start: 5, end: 6 },
@@ -43,15 +46,18 @@ describe('utils', () => {
     let placeholders: string[];
     let pieces: string[];
 
+    // No placeholder
+    [placeholders, pieces] = utils.splitByPlaceholders('func()');
+    expect(placeholders).toEqual([]);
+    expect(pieces).toEqual(['func()']);
+
     // Single placeholder
     [placeholders, pieces] = utils.splitByPlaceholders('func(${x})');
-
     expect(placeholders).toEqual(['x']);
     expect(pieces).toEqual(['func(', ')']);
 
     // Multiple placeholders
     [placeholders, pieces] = utils.splitByPlaceholders('func(${x}, ${x})');
-
     expect(placeholders).toEqual(['x', 'x']);
     expect(pieces).toEqual(['func(', ', ', ')']);
   });
