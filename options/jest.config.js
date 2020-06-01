b055fd414ab185f88b2f75f6aa237f1be239e6a2
harupy <hkawamura0130@gmail.com>
2020-06-01 23:43:35 +0900

    Update config to run enzyme

--- options/jest.config.js
+++ options/jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testMatch: ['**/tests/**/*.test.[jt]s?(x)'],
+  setupFilesAfterEnv: ['<rootDir>src/setupTests.ts'],
+  moduleNameMapper: {
+    '^#/(.+)': '<rootDir>/src/$1',
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
 };
