cdc76bc0e1a5c997c5e43de86d522d80f2910621
harupy <hkawamura0130@gmail.com>
2020-06-02 13:35:39 +0900

    Update tests

--- options/src/tests/components/highlight-placeholders.test.tsx
+++ options/src/tests/components/highlight-placeholders.test.tsx
@@ -2,36 +2,57 @@ import * as React from 'react';
 import { mount, ReactWrapper } from 'enzyme';
 
 import { HighlightPlaceholders } from '#/components/highlight-placeholders';
+import { ByDataTestId } from '#/utils';
 
 describe('HighlightPlaceholders', () => {
   let wrapper: ReactWrapper;
+  const backgroundColor = 'blue';
 
   it('Single placeholder', () => {
     wrapper = mount(
-      <HighlightPlaceholders body="func(${x})" backgroundColor="blue" />,
+      <HighlightPlaceholders
+        body="func(${x})"
+        backgroundColor={backgroundColor}
+      />,
     );
 
     expect(wrapper.text()).toEqual('func(x)');
-    expect(wrapper.find('[data-testid="highlight"]').text()).toEqual('x');
+    expect(wrapper.find(ByDataTestId('highlight')).text()).toEqual('x');
+    wrapper.find(ByDataTestId('highlight')).forEach(node => {
+      expect(node.prop('style')).toHaveProperty(
+        'backgroundColor',
+        backgroundColor,
+      );
+    });
   });
 
   it('Multiple placeholder', () => {
     wrapper = mount(
-      <HighlightPlaceholders body="func(${x}, ${x})" backgroundColor="blue" />,
+      <HighlightPlaceholders
+        body="func(${x}, ${x})"
+        backgroundColor={backgroundColor}
+      />,
     );
 
     expect(wrapper.text()).toEqual('func(x, x)');
-    expect(
-      wrapper.find('[data-testid="highlight"]').map(w => w.text()),
-    ).toEqual(['x', 'x']);
+    expect(wrapper.find(ByDataTestId('highlight')).map(w => w.text())).toEqual([
+      'x',
+      'x',
+    ]);
+    wrapper.find(ByDataTestId('highlight')).forEach(node => {
+      expect(node.prop('style')).toHaveProperty(
+        'backgroundColor',
+        backgroundColor,
+      );
+    });
   });
 
   it('No placeholder', () => {
     wrapper = mount(
-      <HighlightPlaceholders body="func()" backgroundColor="blue" />,
+      <HighlightPlaceholders body="func()" backgroundColor={backgroundColor} />,
     );
 
     expect(wrapper.text()).toEqual('func()');
-    expect(wrapper.find('[data-testid="highlight"]')).toHaveLength(0);
+    expect(wrapper.find(ByDataTestId('highlight'))).toHaveLength(0);
   });
 });
