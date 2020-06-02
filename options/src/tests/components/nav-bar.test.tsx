7e0ca46c49af7e21904f85959f78e1ef05f19659
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-06-02 14:54:58 +0900

    Add test for nav-bar (#50)
    * Add test for nav-bar
    
    * fix
    
    * Fix parentheses

---
+++ options/src/tests/components/nav-bar.test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import { NavBar } from '#/components/nav-bar';
+
+describe('NavBar', () => {
+  it('should render without crashing', () => {
+    const wrapper = shallow(<NavBar />);
+    expect(wrapper).toHaveLength(1);
+  });
+});
